696dad99d4feb22b35fad4c3a36bc6170aebd4f7
Austine <austinelomocso@gmail.com>
2025-10-05 03:34:47 +0800

    settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import Appbar from "./components/Appbar";
 import Students from "./components/Students";
 import Dashboard from "./components/Client/Dashboard/Dashboard";
+import Settings from "./components/Client/Settings/Settings";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Dashboard />
+      <Settings />
     </>
   );
 }
