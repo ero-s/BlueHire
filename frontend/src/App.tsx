ec49df8969e9b08eb1bd130a31e6e78363fa2b98
Keloy <kyleplando234@gmail.com>
2025-10-06 19:33:43 +0800

    Booking/Job and Job Details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import DashboardUpperSection from "./components/DashboardUpperSection/DashboardUpperSection";
-import DashboardMainSection from "./components/DashboardMainSection/DashboardMainSection";  
+// import DashboardUpperSection from "./components/DashboardUpperSection/DashboardUpperSection";
+// import DashboardMainSection from "./components/DashboardMainSection/DashboardMainSection";  
+import BookingJobManagementUppersSection from "./components/BookingJobManagementUpperSection/BookingJobManagementUpperSection";
+import BookingJobManagementMainSection from "./components/BookingJobManagementMainSection/BookingJobManagementMainSection";
+// import JobDetails from "./components/JobDetailsMainSection/JobDetails";
 
 function App() {
   return (
     <>
       <NavBar/>
-      <DashboardUpperSection/>
-      <DashboardMainSection/> 
+      <BookingJobManagementUppersSection/>
+      <BookingJobManagementMainSection/>
     </>
   );
 }
